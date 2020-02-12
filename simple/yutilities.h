2d03f8f97b9492b56a7b2f09f830f4ae4485fbbc
Matthew Boddewyn <mboddewy@u.rochester.edu>
2020-02-11 21:47:07 -0500

    make move_elem visible

--- simple/yutilities.h
+++ simple/yutilities.h
@@ -13,6 +13,7 @@ double get_double(yarr *, int);
 long get_long(yarr *, int);
 float get_float(yarr *, int);
 int get_int(yarr *, int);
+void move_elem(yarr *, int, int);
 void print_C_array(yarr *);
 void print_yarr(yarr *);
 #endif /* YUTILITIES_H */
