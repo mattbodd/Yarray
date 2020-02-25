43b5c5704d2f65054408e873a73c9fee56f0ddee
Matthew Boddewyn <mboddewy@u.rochester.edu>
2020-02-25 14:22:44 -0500

    implement dealloc for `yarr`

--- simple/yarray.c
+++ simple/yarray.c
@@ -1,2 +1,28 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "yarray.h"
+
+void dealloc_yarr(yarr *y) {
+  // Free strides field
+  free(y->strides);
+  // Free correct data type
+  switch (y->tag) {
+  case INT:
+    free(y->data.idata);
+    break;
+  case FLOAT:
+    free(y->data.fdata);
+    break;
+  case LONG:
+    free(y->data.ldata);
+    break;
+  case DOUBLE:
+    free(y->data.ddata);
+    break;
+  default:
+    printf(RED"Unsupported data type while deallocating\n"NC);
+    break;
+  }
+  // Free yarr itself
+  free(y);
+}
