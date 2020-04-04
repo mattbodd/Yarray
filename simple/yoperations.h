3f9236144dc9966ca71f06fda638633164d1f89b
Matthew Boddewyn <mboddewy@u.rochester.edu>
2020-04-04 17:12:50 -0400

    Return type of minimally functionaly `matrix_mult` should be `double`

--- simple/yoperations.h
+++ simple/yoperations.h
@@ -8,7 +8,7 @@ void update_point(yarr *, int, yarr *, int);
 void update_C_array(yarr *, yarr *, int, int *);
 void reform_C_array(yarr *, int *, int);
 void shrink_C_array(yarr *, int *, int, int, int);
-yarr *matrix_mult(yarr *, yarr *);
+double matrix_mult(yarr *, yarr *);
 yarr *broadcast(yarr *, yarr*, Op);
 yarr *apply_op(yarr *, yarr*, Op);
 void *grab_point(yarr *, int *, int);
